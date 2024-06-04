5062e0025c3baff8b37a191b7573bd15353cc605
nilam1995 <72725548+nilam1995@users.noreply.github.com>
2024-06-04 23:14:13 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 
+import { Footer, NavBar} from '../Components'
+
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Footer/>
       <Component {...pageProps} />
+      <NavBar/>
     </div>
   );
 }
