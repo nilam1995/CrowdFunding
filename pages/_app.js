bd1082e6abd1560e4b8b1636917e7a315d56389b
nilam1995 <72725548+nilam1995@users.noreply.github.com>
2024-06-05 02:01:18 +0530

    contract added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 
 import { Footer, NavBar} from '../Components'
+import {CrowdFundingProvider} from '../Context/CrowdFunding'
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <Footer/>
-      <Component {...pageProps} />
-      <NavBar/>
+      <CrowdFundingProvider>
+        <Footer/>
+        <Component {...pageProps} />
+        <NavBar/>
+      </CrowdFundingProvider>
     </div>
   );
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,6 @@
 const hre = require("hardhat");
 
+
 async function main() {
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
   const crowdFunding = await CrowdFunding.deploy();
